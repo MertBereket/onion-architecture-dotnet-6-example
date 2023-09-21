ffde0e2aac53d26c417be485b1c06396712c38f6
Mert <mertbereket80@gmail.com>
2023-09-21 23:29:19 +0300

    feat: Write and Read Repository created

---
+++ Core/ETicaretAPI.Application/Repostories/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using ETicaretAPI.Domain.Entities.Common;
+using Microsoft.EntityFrameworkCore;
+	
+namespace ETicaretAPI.Application.Repostories
+{
+	public interface IRepository<T> where T : BaseEntity
+	{
+		DbSet<T> Table { get; }
+	}
+}
+
---
+++ Core/ETicaretAPI.Application/Repostories/IWriteRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ETicaretAPI.Domain.Entities.Common;
+
+namespace ETicaretAPI.Application.Repostories
+{
+	public interface IWriteRepository<T> : IRepository<T> where T : BaseEntity
+	{
+		Task<bool> AddAsync(T model);
+        Task<bool> AddRangeAsync(List<T> datas);
+		bool Remove(T model);
+        Task<bool> RemoveAsync(string id);
+		bool RemoveRange(List<T> datas);
+		bool Update(T model);
+		Task<int> SaveAsync();
+    }
+}
+
