82e9492be269d1e66fde6e96618fc495e9212a6e
Mert <mertbereket80@gmail.com>
2023-09-21 23:31:54 +0300

    feat: db context created

---
+++ Infrastructure/ETicaretAPI.Persistence/DesingTimeDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ETicaretAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ETicaretAPI.Persistence
+{
+    public class DesingTimeDbContextFactory : IDesignTimeDbContextFactory<ETicaretAPIDbContext>
+    {
+        public ETicaretAPIDbContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<ETicaretAPIDbContext> dbContextOptionsBuilder = new();
+            dbContextOptionsBuilder.UseNpgsql(Configuration.ConnectionString);
+            return new(dbContextOptionsBuilder.Options);
+        }
+    }
+}
+
---
+++ Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using ETicaretAPI.Persistence.Contexts;
+using Microsoft.Extensions.Configuration;
+using ETicaretAPI.Persistence.Repostories.Customers;
+using ETicaretAPI.Application.Repostories.Customers;
+using ETicaretAPI.Application.Repostories.Orders;
+using ETicaretAPI.Persistence.Repostories.Orders;
+using ETicaretAPI.Application.Repostories.Products;
+using ETicaretAPI.Persistence.Repostories.Products;
+
+namespace ETicaretAPI.Persistence
+{
+	public static class ServiceRegistration
+	{
+		public static void AddPersistenceServices(this IServiceCollection services)
+		{
+			//services.AddSingleton<IProductService, ProductService>();
+			services.AddDbContext<ETicaretAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString)) ;
+			services.AddScoped<ICustomerReadRepository, CustomerReadRepository>();
+			services.AddScoped<ICustomerWriteRepository, CustomerWriteRepsitory>();
+			services.AddScoped<IOrderReadRepository, OrderReadRepository>();
+			services.AddScoped<IOrderWriteRepository, OrderWriteRepository>();
+			services.AddScoped<IProductReadRepository, ProductReadRepository>();
+			services.AddScoped<IProductWriteRepository, ProductWriteRepository>();
+		}
+	}
+}
+
