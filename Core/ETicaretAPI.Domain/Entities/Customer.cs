a14b7c013feb31fc3b341886cba699e14f11b84f
Mert <mertbereket80@gmail.com>
2023-09-21 23:30:02 +0300

    feat: entities created

---
+++ Core/ETicaretAPI.Domain/Entities/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using ETicaretAPI.Domain.Entities.Common;
+
+namespace ETicaretAPI.Domain.Entities
+{
+	public class Customer : BaseEntity
+	{
+		public ICollection<Order> Orders { get; set; }
+		public string Name { get; set; }
+	}
+}
+
